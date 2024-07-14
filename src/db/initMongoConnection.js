615f36b2e7ad8c383e07cf351c771e2af67f1769
MykolaNovokhatko <143940513+MykolaNovokhatko@users.noreply.github.com>
2024-07-14 13:20:49 +0100

    hw2 7/7

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,20 +1,20 @@
-import 'dotenv/config';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 async function initMongoConnection() {
-  const { MONGODB_USER, MONGODB_PASSWORD, MONGODB_URL, MONGODB_DB } =
-    process.env;
+  const { MONGODB_USER, MONGODB_PASSWORD, MONGODB_URL, MONGODB_DB } = process.env;
   const DB_URI = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URL}/${MONGODB_DB}?retryWrites=true&w=majority`;
 
   try {
-    // console.log({DB_URI});
-    await mongoose.connect(DB_URI);
-    console.log('Mongo connection successfully established!');
+    await mongoose.connect(DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connection successfully established!');
   } catch (error) {
-    console.error(error);
+    console.error('Error connecting to MongoDB:', error);
     process.exit(1);
   }
 }
