d507420a3a20864efc369da943331cd5b185311d
MykolaNovokhatko <143940513+MykolaNovokhatko@users.noreply.github.com>
2024-07-14 14:01:58 +0100

    hw2 7/7

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -4,12 +4,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 async function initMongoConnection() {
-  const { DB_URI } = process.env;
-  
-  if (!DB_URI) {
-    console.error('DB_URI is not defined');
-    process.exit(1);
-  }
+  const { MONGODB_USER, MONGODB_PASSWORD, MONGODB_URL, MONGODB_DB } = process.env;
+  const user = MONGODB_USER;
+  const pwd = MONGODB_PASSWORD;
+  const url = MONGODB_URL;
+  const db = MONGODB_DB;
+
+  const DB_URI = `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`;
 
   try {
     await mongoose.connect(DB_URI, {
