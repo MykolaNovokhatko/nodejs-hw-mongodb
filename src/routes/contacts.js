78fd0b8ff504ebf6a30bdfd6cdd47f3a7943da89
MykolaNovokhatko <143940513+MykolaNovokhatko@users.noreply.github.com>
2024-07-21 20:02:33 +0100

    hw3-crud

--- src/routes/contacts.js
+++ src/routes/contacts.js
@@ -1,12 +1,19 @@
 import express from 'express';
-import { getContacts, getContactByIdController, createContact, updateContact, deleteContact } from '../controllers/controllerContacts.js';
+import {
+  getContacts,
+  getContactByIdController,
+  createContactController,
+  updateContactController,
+  deleteContactController,
+} from '../controllers/controllerContacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+
 const router = express.Router();
 
-router.get('/', getContacts);
-router.get('/:contactId', getContactByIdController);
-router.post('/', ctrlWrapper(createContact));
-router.patch('/:contactId', ctrlWrapper(updateContact));
-router.delete('/:contactId', ctrlWrapper(deleteContact));
+router.get('/', ctrlWrapper(getContacts));
+router.get('/:contactId', ctrlWrapper(getContactByIdController));
+router.post('/', ctrlWrapper(createContactController));
+router.patch('/:contactId', ctrlWrapper(updateContactController));
+router.delete('/:contactId', ctrlWrapper(deleteContactController));
 
 export default router;
